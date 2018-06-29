7361e6917f92704d2d6f0edb025e428ec2cd3497
Zack Bloom <zack@cloudflare.com>
2018-06-28 19:12:36 -0700

    Normalize PBKDF2 tests to SHA512

--- pbkdf2/lambda.js
+++ pbkdf2/lambda.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto')
 
 exports.handler = async (event) => {
-  const key = crypto.pbkdf2Sync(crypto.randomBytes(16), crypto.randomBytes(16), 15000, 32, 'sha256')
+  const key = crypto.pbkdf2Sync(crypto.randomBytes(16), crypto.randomBytes(16), 15000, 32, 'sha512')
 
   return {
    "isBase64Encoded": false,
--- pbkdf2/lambda_at_edge.js
+++ pbkdf2/lambda_at_edge.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto')
 
 exports.handler = (event, context, callback) => {
-  const key = crypto.pbkdf2Sync(crypto.randomBytes(16), crypto.randomBytes(16), 15000, 32, 'sha256')
+  const key = crypto.pbkdf2Sync(crypto.randomBytes(16), crypto.randomBytes(16), 15000, 32, 'sha512')
 
   const response = {
     status: '200',
